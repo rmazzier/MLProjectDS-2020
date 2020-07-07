103d455d166ee825f339f60bb02a0eaf6d3cfc91
Vittorino Squarcina <giuliano.squarcina@gmail.com>
2020-07-07 17:00:25 +0200

    add more comments

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -10,14 +10,13 @@ x_valid=x_valid/255
 # Implement you model here
 ##################################################
 
-
 import joblib
 
 ###### KNN #####
 #scaling the data (StandardScaler & MinMaxScaler tested) is maybe not really necessary
-#since all feature have the same unit of measurement (int in [0;255]).
+#since all features have the same unit of measurement (int in [0;255]).
 #Trying with and without standardization, the train and valid set perfomance is
-#the same.
+#the same, so we do not apply any standardization.
 from sklearn.preprocessing import StandardScaler,MinMaxScaler
 scaler = StandardScaler()#MinMaxScaler()
 scaler.fit(x_train)
@@ -35,7 +34,16 @@ x_valid=x_valid[0:1000]
 y_valid=y_valid[0:1000]
 #*******************************************************************************
 
-#use grid search to find the best value of k
+#use grid search to find the best value of k (='n_neighbors'). 
+#We do not look for the best value of 'p' in the L_p metrics, because theorical 
+#results have prooved that the meaningfulness of the L_p norm worsens faster within 
+#increasing dimensionality for higher values of p. Thus, for a given problem with a 
+#fixed (high) value for the dimensionality d, it is preferable to use lower values of p.
+#This means that the L1 distance metric (Manhattan distance metric) is the most preferable 
+#for high dimensional applications, followed by the Euclidean metric (L2).  
+#For reference see:
+#Charu C. Aggarwal, Alexander Hinneburg, Daniel A. Keim, "On the Surprising Behavior of Distance Metrics in High Dimensional Space"
+#Kevin Beyer, Jonathan Goldstein, Raghu Ramakrishnan, Uri Shaft, "When Is 'Nearest Neighbor' Meaningful?"
 from sklearn.neighbors import KNeighborsClassifier
 param_grid = {'n_neighbors': list(range(1,21))}
 from sklearn.model_selection import GridSearchCV
@@ -43,9 +51,6 @@ from sklearn.model_selection import StratifiedKFold
 cv=StratifiedKFold(n_splits=5,shuffle=True,random_state=0)
 grid_search = GridSearchCV(KNeighborsClassifier(p=1), param_grid, cv=cv,n_jobs=-1)
 grid_search.fit(x_train, y_train)
-print("Best parameters: {}".format(grid_search.best_params_))
-print("Best cross-validation score: {:.2f}".format(grid_search.best_score_))
-print("Valid set score: {:.2f}".format(grid_search.score(x_valid, y_valid)))
 
 # save the model to disk
 filename = 'grid_search.sav'
@@ -55,18 +60,27 @@ joblib.dump(grid_search, filename)
 filename = 'grid_search.sav'
 grid_search = joblib.load(filename)
 
-results = pd.DataFrame(grid_search.cv_results_)
-results=results[['params','mean_test_score','rank_test_score']]
+# convert the grid_search model in a dataframe to see easely the results
+results_grid_search_knn = pd.DataFrame(grid_search.cv_results_)
+results_grid_search_knn=results_grid_search_knn[['params','mean_test_score','rank_test_score']]
+# get the optimal hyperparameter 'n_neighboors'
 k_star=grid_search.best_params_['n_neighbors']
 
+#summary of model and performaces
+print("Best estimator:\n{}".format(grid_search.best_estimator_))
+print("Best parameters: {}".format(grid_search.best_params_))
+print("Best cross-validation score: {:.3f}".format(grid_search.best_score_))
+print("Validation set score: {:.3f}".format(grid_search.score(x_valid, y_valid)))
 
-#to recognize an item, we don't look at the single pixel, but at the overall image.
-#So extracting PCA and using them to identify different items is an attempt to mimic
-#how humans identify items.
+
+
+#To recognize an item, we don't look at the single pixel, but at the overall image.
+#So extracting Principal Components (PCs) and using them to identify different items 
+#is an attempt to mimic how humans identify items.
 #Using the optimal value of k found before, we look how much varies the accuracy
-#in train and valid set increasing the n° of PC used to approximate each sample.
+#in train and valid set increasing the n° of PCs used to approximate each sample.
 #Around 20 PCs, the Accuracy curve starts being flat, meaning that adding further 
-#PCs increases just a little the performance, so no worth to add them.
+#PCs increases just a little the performance, so it's not worth to add them.
 from sklearn.decomposition import PCA
 test_accuracy = []
 PC_number = range(1, 101)
@@ -83,17 +97,27 @@ plt.plot(PC_number, test_accuracy, label="valid accuracy")
 plt.ylabel("Accuracy")
 plt.xlabel("n° PC")
 plt.legend()  
+
+#save the figure
 plt.savefig('Accuracy VS number_PCs.png')
+
+#load the figure
+import matplotlib.image as mpimg
+img = mpimg.imread('Accuracy VS number_PCs.png')
+#fig=plt.figure()
+#ax=fig.add_subplot(1,1,1)
+plt.axis('off')
+plt.imshow(img,interpolation='spline36')
 
 #optimal number of PC
 PCs_star=np.argmax(test_accuracy)+1
 #observing the plot, the accuracy growth becomes flat approx. after the 
-#first 20 PCs. So to have an easier model, with less feature, we 
+#first 20 PCs. So to have an easier model, with less features, we 
 #decide to use 20 PCs.
 PCs_star=20
 valid_set_accuracy=test_accuracy[PCs_star-1]
 
-#train the model with k=k_star and PCs_star=20
+#compute the first 20 PCs for x_train and x_valid
 pca = PCA(n_components=20, whiten=True, random_state=0).fit(x_train)
 x_train_pca = pca.transform(x_train)
 x_valid_pca = pca.transform(x_valid)
@@ -106,6 +130,7 @@ joblib.dump(pca, filename)
 filename = 'pca.sav'
 pca = joblib.load(filename)
 
+#train a knn model with k=k_star and n° PCs=20
 knn = KNeighborsClassifier(n_neighbors=k_star,p=1)
 knn.fit(x_train_pca, y_train)
 valid_set_accuracy_20=knn.score(x_valid_pca, y_valid.reshape(-1,1))
@@ -126,8 +151,10 @@ for i, (component, ax) in enumerate(zip(pca.components_, axes.ravel())):
     ax.set_title("{}. component".format((i + 1)))
 plt.savefig('first 20 PCs.png')
 
-
-
+#load the figure
+img = mpimg.imread('first 20 PCs.png')
+plt.axis('off')
+plt.imshow(img,interpolation='spline36')
 
 ##################################################
 # Evaluate the model here
@@ -147,7 +174,11 @@ valid_set_accuracy_20_helper=accuracy(y_pred,y_valid)
 
 
 
+
 ####### Decision Tree & Random Forest using PCs #####
+
+#Using PCA, we reduced the features' dimension from 784 to 20. Now, we want to apply
+#a 'Decision Tree' to classify the items using only the 20 PCs extracted before.
 from sklearn.tree import DecisionTreeClassifier
 #choosing the parameter 'max_depth' high enough, it's possible to classify 
 #perfectly each class in the training set, reaching an accuracy of 100%. But
@@ -155,10 +186,16 @@ from sklearn.tree import DecisionTreeClassifier
 tree = DecisionTreeClassifier(max_depth=15,random_state=0)
 tree.fit(x_train_pca, y_train)
 print("Accuracy on training set: {:.3f}".format(tree.score(x_train_pca, y_train)))
-print("Accuracy on valid set: {:.3f}".format(tree.score(x_valid_pca, y_valid)))
-#
+print("Accuracy on validation set: {:.3f}".format(tree.score(x_valid_pca, y_valid)))
+
+#A random forest is essentially a collection of decision trees, where each tree is slightly different from
+#the others. The idea behind random forests is that each tree might do a relatively
+#good job of predicting, but will likely overfit on part of the data. If we build many
+#trees, all of which work well and overfit in different ways, we can reduce the amount
+#of overfitting by averaging their results. 
 from sklearn.ensemble import RandomForestClassifier
-#'n_estimators':number of trees to build(larger is always better. Averaging more trees will yield a more robust ensemble by reducing overfitting.)
+#'n_estimators':number of trees to build(larger is always better. Averaging more trees will
+#                                        yield a more robust ensemble by reducing overfitting.)
 #'max_features': number of randomly selected features among which the algo looks for the best one
 #                it’s a good rule of thumb to use the default values: max_features=sqrt(n_features) 
 forest = RandomForestClassifier(n_estimators=100,
@@ -181,9 +218,6 @@ grid_search = GridSearchCV(RandomForestClassifier(n_estimators=100,
                                                   n_jobs=-1),
                            param_grid, cv=cv,n_jobs=-1)
 grid_search.fit(x_train_pca, y_train)
-print("Best parameters: {}".format(grid_search.best_params_))
-print("Best cross-validation score: {:.2f}".format(grid_search.best_score_))
-print("Valid set score: {:.2f}".format(grid_search.score(x_valid_pca, y_valid)))
 
 # save the model to disk
 filename = 'grid_search_forest.sav'
@@ -193,8 +227,15 @@ joblib.dump(grid_search, filename)
 filename = 'grid_search_forest.sav'
 grid_search = joblib.load(filename)
 
-results = pd.DataFrame(grid_search.cv_results_)
-results=results[['params','mean_test_score','rank_test_score']]
+# convert the grid_search model in a dataframe to see easely the results
+results_grid_search_randForest = pd.DataFrame(grid_search.cv_results_)
+results_grid_search_randForest=results[['params','mean_test_score','rank_test_score']]
+
+#summary of model and performaces
+print("Best estimator:\n{}".format(grid_search.best_estimator_))
+print("Best parameters: {}".format(grid_search.best_params_))
+print("Best cross-validation score: {:.2f}".format(grid_search.best_score_))
+print("Validation set score: {:.2f}".format(grid_search.score(x_valid_pca, y_valid)))
 
 #train the model with optimal hyperparameters and test it
 forest = RandomForestClassifier(n_estimators=100,
@@ -207,10 +248,10 @@ print("Accuracy on training set: {:.3f}".format(forest.score(x_train_pca, y_trai
 print("Accuracy on valid set: {:.3f}".format(forest.score(x_valid_pca, y_valid)))
 
 #problem: the optimal solution is a corner one: max_depth=20
-#to be sure that do not exist a better model with higher depth, we expand
+#to be sure that does not exist a better model with higher depth, we expand
 #the grid search to values of max_depth higher than 20
 
-#use RESTRICTCED grid search to find the best value of 'max_depth' and 'max_features'
+#use extension of grid search to find the best value of 'max_depth' and 'max_features'
 param_grid = {'max_depth': list(range(20,30)),
               'max_features': list(range(5,11))}
 from sklearn.model_selection import GridSearchCV
@@ -221,9 +262,6 @@ grid_search = GridSearchCV(RandomForestClassifier(n_estimators=100,
                                                   n_jobs=-1),
                            param_grid, cv=cv,n_jobs=-1)
 grid_search.fit(x_train_pca, y_train)
-print("Best parameters: {}".format(grid_search.best_params_))
-print("Best cross-validation score: {:.2f}".format(grid_search.best_score_))
-print("Valid set score: {:.2f}".format(grid_search.score(x_valid_pca, y_valid)))
 
 # save the model to disk
 filename = 'grid_search_restricted_forest.sav'
@@ -233,8 +271,16 @@ joblib.dump(grid_search, filename)
 filename = 'grid_search_restricted_forest.sav'
 grid_search = joblib.load(filename)
 
-results2 = pd.DataFrame(grid_search.cv_results_)
-results2=results2[['params','mean_test_score','rank_test_score']]
+# convert the grid_search model in a dataframe to see easely the results
+results_grid_search_randForest = pd.DataFrame(grid_search.cv_results_)
+results_grid_search_randForest=results_grid_search_randForest[['params','mean_test_score','rank_test_score']]
+
+
+#summary of model and performaces
+print("Best estimator:\n{}".format(grid_search.best_estimator_))
+print("Best parameters: {}".format(grid_search.best_params_))
+print("Best cross-validation score: {:.2f}".format(grid_search.best_score_))
+print("Validation set score: {:.2f}".format(grid_search.score(x_valid_pca, y_valid)))
 
 #train the model with optimal hyperparameters and test it
 forest = RandomForestClassifier(n_estimators=100,
@@ -246,8 +292,8 @@ forest.fit(x_train_pca, y_train)
 print("Accuracy on training set: {:.3f}".format(forest.score(x_train_pca, y_train)))
 print("Accuracy on valid set: {:.3f}".format(forest.score(x_valid_pca, y_valid)))
 
-#Anyway the major advantage of Tree is interpretability, so using a Tree when features have not 
-#a clear interpretation (because are PCs) is not meaningfull.
+#Anyway the major advantage of Tree is interpretability, so using a Decision Tree when 
+#features have not a clear interpretation (because are PCs) is not meaningfull.
 #Better to use KNN with PCs.
 
 ##To Be Done (Maybe!!!)
